709c7d48f3ec6013f78b4d218ac980c6789a4526
dthung1602 <dthung1602@gmail.com>
2021-01-01 11:48:47 +0700

    Clean up

--- constants.py
+++ constants.py
@@ -5,10 +5,9 @@ ROOT_DIR = os.path.dirname(__file__)
 RAW_DATA_DIR = os.path.join(ROOT_DIR, "data", "raw")
 PROCESSED_DATA_DIR = os.path.join(ROOT_DIR, "data", "processed")
 
-AI_TEXT_GEN_VOCAB_DIR = os.path.join(ROOT_DIR, "aitextgen")
-AI_TEXT_GEN_VOCAB_FILE = os.path.join(AI_TEXT_GEN_VOCAB_DIR, "aitextgen-vocab.json")
-AI_TEXT_GEN_MERGES_FILE = os.path.join(AI_TEXT_GEN_VOCAB_DIR, "aitextgen-merges.txt")
-AI_TEXT_GEN_DATASET_DIR = os.path.join(ROOT_DIR, "data", "dataset")
+AI_TEXT_GEN_TOKENIZED_DIR = os.path.join(PROCESSED_DATA_DIR, "tokenized")
+AI_TEXT_GEN_VOCAB_FILE = os.path.join(AI_TEXT_GEN_TOKENIZED_DIR, "vocab.json")
+AI_TEXT_GEN_MERGES_FILE = os.path.join(AI_TEXT_GEN_TOKENIZED_DIR, "merges.txt")
 AI_TEXT_GEN_VOCAB_SIZE = 10000
 AI_TEXT_GEN_BLOCK_SIZE = 1024
 
@@ -21,5 +20,5 @@ class BookSet(Enum):
     HCM = 'HO CHI MINH'
     LENIN = 'LENIN'
     MAC = 'CAC MAC'
-    VK_DANG = 'VK DANG'
+    VK_DANG = 'VK Dang'
     OTHER = 'OTHER'
--- create_token_datasets.py
+++ create_token_datasets.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import subprocess
 
 from aitextgen.TokenDataset import TokenDataset
 from aitextgen.tokenizers import train_tokenizer
@@ -18,16 +19,31 @@ def create_dataset(txt_file: str):
         merges_file=AI_TEXT_GEN_MERGES_FILE,
         vocab_file=AI_TEXT_GEN_VOCAB_FILE,
         save_cache=True,
-        cache_destination=os.path.join(AI_TEXT_GEN_DATASET_DIR, get_plain_file_name(txt_file) + ".tar.gz"),
+        cache_destination=os.path.join(AI_TEXT_GEN_TOKENIZED_DIR, get_plain_file_name(txt_file) + ".tar.gz"),
         block_size=AI_TEXT_GEN_BLOCK_SIZE
     )
 
 
+def combine_txt_files():
+    processes = []
+    for bookset in BookSet:
+        combined_file = f"_{bookset.value.upper().replace(' ', '')}.TXT"
+        pattern = bookset.value.replace(' ', '\\ ') + '*'
+        proc = subprocess.Popen(f"cat {pattern} > {combined_file}", shell=True, cwd=PROCESSED_DATA_DIR)
+        processes.append(proc)
+
+    [proc.wait() for proc in processes]
+
+    subprocess.Popen(f"cat _* > _ALL.TXT", shell=True, cwd=PROCESSED_DATA_DIR).wait()
+
+
 def main():
+    combine_txt_files()
+
     train_tokenizer(
         os.path.join(PROCESSED_DATA_DIR, "_ALL.TXT"),
         vocab_size=AI_TEXT_GEN_VOCAB_SIZE,
-        save_path=AI_TEXT_GEN_VOCAB_DIR
+        save_path=AI_TEXT_GEN_TOKENIZED_DIR
     )
 
     txt_files = [os.path.join(PROCESSED_DATA_DIR, f) for f in os.listdir(PROCESSED_DATA_DIR) if f.endswith(".TXT")]
